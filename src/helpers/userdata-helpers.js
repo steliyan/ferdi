342f3bb241bd4d80838672144d9eccaec09a9239
vantezzen <hello@vantezzen.io>
2019-10-29 21:34:47 +0100

    Improve path data setup

---
+++ src/helpers/userdata-helpers.js
@@ -0,0 +1,17 @@
+import {
+  app
+} from 'electron';
+import path from 'path';
+
+export function setUpDataPath() {
+  if (process.env.FERDI_APPDATA_DIR != null) {
+    app.setPath('appData', process.env.FERDI_APPDATA_DIR);
+    app.setPath('userData', path.join(app.getPath('appData')));
+  } else if (process.env.PORTABLE_EXECUTABLE_DIR != null) {
+    app.setPath('appData', process.env.PORTABLE_EXECUTABLE_DIR, `${app.getName()}AppData`);
+    app.setPath('userData', path.join(app.getPath('appData'), `${app.getName()}AppData`));
+  } else if (process.platform === 'win32') {
+    app.setPath('appData', process.env.APPDATA);
+    app.setPath('userData', path.join(app.getPath('appData'), app.getName()));
+  }
+}
